4b3dd2ff3a018c26e294eff7cddaa4eb1c36d2bc
MilenaVieira06 <113488407+MilenaVieira06@users.noreply.github.com>
2022-09-28 21:56:36 -0300

    Add files via upload

---
+++ código.js
@@ -0,0 +1,10 @@
+var nome 
+        nome = window.prompt("Digite seu nome:");
+        document.write("Bom dia, " + nome + "!<br>");
+        document.write("Tchau!!");
+        window.alert("Nosso script de javascript na tela!!!");
+        var num1 = 10
+        var num2 = 30
+        var num3 = 0
+        soma = num1 + num2
+        alert("O valor do cálculo é: " + soma);
---
+++ código1.js
@@ -0,0 +1,31 @@
+var n1 = 4
+var n2 = 8
+var n3 = 7
+var n4 = 5
+var media = 0
+var soma = 0
+var media = (n1 + n2 + n3 + n4)/4
+if(media >= 7)
+{
+    alert("A média é: " + media + " O aluno está aprovado!");
+}
+else
+{
+    alert("A média é: " + media + " O aluno não está aprovado!");
+}
+if(media >= 5 & media < 7)
+{
+    alert("A média é: " + media + " O aluno está em recuperação!");
+}
+else
+{
+    alert("A média é: " + media + " O aluno não está em recuperação!");
+}
+if(media <= 5)
+{
+    alert("A média é: " + media + " O aluno está reprovado!");
+}
+else
+{
+    alert("A média é: " + media + " O aluno não está reprovado!");
+}
